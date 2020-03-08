e27449442a8ca37b16de8db60ce82a6626a30c56
Hung <mw3tv123@gmail.com>
2020-03-08 23:59:53 +0700

    Initiated.

---
+++ Data/City.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherForcast.Data {
+    public class City {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
---
+++ Services/JSON.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WeatherForcast.Services {
+    public static class JSON {
+        public static object ToCollections ( object obj ) {
+            if ( obj is JObject jObj )
+                return jObj.ToObject<Dictionary<string, object>>()
+                           .ToDictionary(k => k.Key, v => ToCollections(v.Value));
+            if ( obj is JArray jArr )
+                return jArr.ToObject<List<object>>()
+                           .Select(ToCollections)
+                           .ToList();
+            return obj;
+        }
+
+        public static object ToObject( string json ) => JsonConvert.DeserializeObject(json);
+    }
+}
