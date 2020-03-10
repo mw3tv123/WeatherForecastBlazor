0b6cdd4a362e8389973f12625cb8b4a18ec310d2
Hung <mw3tv123@gmail.com>
2020-03-10 21:42:13 +0700

    Fixed Geolocation API and Weather API updateing loop.

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -13,6 +13,8 @@ namespace WeatherForcast.Data
         public double TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+        public string IconLink { get; set; }
     }
 
     public enum TemperatureType
