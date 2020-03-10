1f6395f06a1226b58f676e97568b558797a633d5
Hung <mw3tv123@gmail.com>
2020-03-10 08:42:22 +0700

    Added JSON Helper. Added Weather Forecast API response handler.

--- Services/JSON.cs
+++ Services/JSON.cs
@@ -1,21 +1,21 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json.Linq;
 using System.Linq;
 
 namespace WeatherForcast.Services {
     public static class JSON {
-        public static object ToCollections ( object obj ) {
-            if ( obj is JObject jObj )
-                return jObj.ToObject<Dictionary<string, object>>()
-                           .ToDictionary(k => k.Key, v => ToCollections(v.Value));
-            if ( obj is JArray jArr )
-                return jArr.ToObject<List<object>>()
-                           .Select(ToCollections)
-                           .ToList();
-            return obj;
-        }
+        public static object Deserialize( string json ) => ToObject(JToken.Parse(json));
 
-        public static object ToObject( string json ) => JsonConvert.DeserializeObject(json);
+        public static object ToObject( JToken token ) {
+            switch (token.Type) {
+                case JTokenType.Object:
+                    return token.Children<JProperty>()
+                                .ToDictionary(prop => prop.Name,
+                                              prop => ToObject(prop.Value));
+                case JTokenType.Array:
+                    return token.Select(ToObject).ToList();
+                default:
+                    return ((JValue)token).Value;
+            }
+        }
     }
 }
